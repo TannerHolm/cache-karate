b35cd38e64b6526adaa62f76d816ee7a9b1d6386
TannerHolm <holm.tanner@gmail.com>
2014-08-25 10:58:25 -0600

    tabs

--- js/agency.js
+++ js/agency.js
@@ -2,7 +2,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 100
         }, 900, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -17,42 +17,21 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
 $('#myTab a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
 
-// var cbpAnimatedHeader = (function() {
 
-    // var docElem = document.documentElement,
-    //     header = document.querySelector( '.navbar-default' ),
-    //     didScroll = false,
-    //     changeHeaderOn = 000;
 
-    // function init() {
-    //     window.addEventListener( 'scroll', function( event ) {
-    //         if( !didScroll ) {
-    //             didScroll = true;
-    //             setTimeout( scrollPage, 300 );
-    //         }
-    //     }, false );
-    // }
-
-//     function scrollPage() {
-//         var sy = scrollY();
-//         if ( sy >= changeHeaderOn ) {
-//             classie.add( header, 'navbar-fixed-top' );
-//         }
-//         else {
-//             classie.remove( header, 'navbar-fixed-top' );
-//         }
-//         didScroll = false;
-//     }
-
-//     function scrollY() {
-//         return window.pageYOffset || docElem.scrollTop;
-//     }
-
-//     init();
-
-// })();
+// $('.tab').on('click',function (e) {
+//     e.preventDefault();
+//     var target = this.hash,
+//     $target = $(target);
+//     $('html, body').stop().animate({
+//       'scrollTop': $target.offset().top
+//     }, 1500, 'easeInOutExpo', function () {
+//       window.location.hash = target;
+//     });
+//   });
